5d126708cb887f7da0c0260f447ae55965a52395
ibacharnikov <53704523+Bachar-official@users.noreply.github.com>
2023-03-22 16:55:27 +0500

    Generate file

---
+++ lib/data/entity/qso.g.dart
@@ -0,0 +1,62 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'qso.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class QSOAdapter extends TypeAdapter<QSO> {
+  @override
+  final int typeId = 0;
+
+  @override
+  QSO read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return QSO(
+      operator: fields[0] as String,
+      call: fields[1] as String,
+      qsoDate: fields[2] as String,
+      timeOn: fields[3] as String,
+      mode: fields[4] as Mode,
+      rstSent: fields[5] as String,
+      rstRcvd: fields[6] as String,
+      band: fields[7] as Band,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, QSO obj) {
+    writer
+      ..writeByte(8)
+      ..writeByte(0)
+      ..write(obj.operator)
+      ..writeByte(1)
+      ..write(obj.call)
+      ..writeByte(2)
+      ..write(obj.qsoDate)
+      ..writeByte(3)
+      ..write(obj.timeOn)
+      ..writeByte(4)
+      ..write(obj.mode)
+      ..writeByte(5)
+      ..write(obj.rstSent)
+      ..writeByte(6)
+      ..write(obj.rstRcvd)
+      ..writeByte(7)
+      ..write(obj.band);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is QSOAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
